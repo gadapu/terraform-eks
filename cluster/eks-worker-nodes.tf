4396f7ceb74969e169f02e50bd3fcc5597ea460a
satheeshchepuri <chepurusatheesh@gmail.com>
2019-04-27 21:03:36 +0530

    Update eks-worker-nodes.tf

--- cluster/eks-worker-nodes.tf
+++ cluster/eks-worker-nodes.tf
@@ -130,7 +130,7 @@ resource "aws_launch_configuration" "demo" {
   associate_public_ip_address = true
   iam_instance_profile        = "${aws_iam_instance_profile.demo-node.name}"
   image_id                    = "${data.aws_ami.eks-worker.id}"
-  instance_type               = "m4.large"
+  instance_type               = "m4.medium"
   name_prefix                 = "terraform-eks-demo"
   security_groups             = ["${aws_security_group.demo-node.id}"]
   user_data_base64            = "${base64encode(local.demo-node-userdata)}"
@@ -143,7 +143,7 @@ resource "aws_launch_configuration" "demo" {
 resource "aws_autoscaling_group" "demo" {
   desired_capacity     = 2
   launch_configuration = "${aws_launch_configuration.demo.id}"
-  max_size             = 2
+  max_size             = 10
   min_size             = 1
   name                 = "terraform-eks-demo"
   vpc_zone_identifier  = ["${aws_subnet.demo.*.id}"]
